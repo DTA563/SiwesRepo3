3efa8927cef62904ffb61b44911430a9f175cbde
DTA563 <76204542+DTA563@users.noreply.github.com>
2025-05-23 15:21:12 +0100

    added sql database

--- my-node-app/app.js
+++ my-node-app/app.js
@@ -1,5 +1,17 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const mysql = require('mysql');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Dav1D@2105',
+    database: 'babcockdb'
+  }
+);
+
+db.connect();
 
 const app = express();
 
@@ -54,6 +66,30 @@ app.get('/mySpace', (req, res) => {
 app.get('/sendMessage', (req, res) =>{
     const message = req.query.message;
     res.send('The response page' + message);
+})
+
+app.get('/department', (req, res) => {
+  const sql = 'insert into department (name, school_id, created_at) values ?';
+
+  const values = [
+    ['computer information system', 1, '2025-05-22 23:30:05'],
+    ['computer technology', 1, '2025-05-22 23:30:05'],
+    ['accounting', 2, '2025-05-22 23:30:05'],
+    ['economics', 3, '2025-05-22 23:30:05']
+  ];
+
+  db.query(sql, [values], (error, result) =>{
+    if(error){
+      console.log(`failed to insert department ${error}`);
+    } else {
+      console.log('departments inserted successfully.');
+    }
+    console.log(result);
+  })
+  
+  db.query('select * from department', (error, result) =>{
+  res.send(result);
+  })
 })
 
 app.listen(port, () => {
